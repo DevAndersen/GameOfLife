7d6801078a4cb6b56c13041e3d1d2c2045c7baad
DevAndersen <martinba.private@gmail.com>
2017-02-12 06:39:00 +0100

    Copied project files into repo.

---
+++ GameOfLife/LifeLib/LifeGrid.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeLib
+{
+    public class LifeGrid
+    {
+        public delegate void UpdateArg(LifeGrid grid);
+        public event UpdateArg OnUpdate;
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        private bool[,] cells;
+        private Random rand;
+
+        public LifeGrid(int width, int height, int? seed = null)
+        {
+            Width = width;
+            Height = height;
+            cells = new bool[width, height];
+            if (seed == null)
+                rand = new Random();
+            else
+                rand = new Random(seed.Value);
+        }
+
+        public void Update()
+        {
+            NextTick();
+            OnUpdate?.Invoke(this);
+        }
+
+        private void NextTick()
+        {
+            bool[,] newCells = new bool[Width, Height];
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    bool alive = cells[x, y];
+                    int aliveAround = 0;
+
+                    if (x < Width - 1 && cells[x + 1, y]) aliveAround++;
+                    if (x > 0 && cells[x - 1, y]) aliveAround++;
+                    if (y < Height - 1 && cells[x, y + 1]) aliveAround++;
+                    if (y > 0 && cells[x, y - 1]) aliveAround++;
+
+                    if (x < Width - 1 && y < Height - 1 && cells[x + 1, y + 1]) aliveAround++;
+                    if (x < Width - 1 && y > 0 && cells[x + 1, y - 1]) aliveAround++;
+                    if (x > 0 && y < Height - 1 && cells[x - 1, y + 1]) aliveAround++;
+                    if (x > 0 && y > 0 && cells[x - 1, y - 1]) aliveAround++;
+
+                    if (alive)
+                    {
+                        if (aliveAround < 2)
+                            newCells[x, y] = false;
+                        else if (aliveAround > 3)
+                            newCells[x, y] = false;
+                        else
+                            newCells[x, y] = true;
+                    }
+                    else
+                    {
+                        if (aliveAround == 3)
+                            newCells[x, y] = true;
+                    }
+                }
+            }
+            cells = newCells;
+        }
+
+        public bool GetCell(int x, int y)
+        {
+            return cells[x, y];
+        }
+
+        public void SetCell(int x, int y, bool isAlive)
+        {
+            cells[x, y] = isAlive;
+        }
+    }
+}
---
+++ GameOfLife/WpfImplementation/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using LifeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfImplementation
+{
+    public partial class MainWindow : Window
+    {
+        LifeGrid lifeGrid;
+        Border[,] cells;
+        bool autoTick = false;
+        Thread autoTickThread;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int width = 32;
+            int height = 32;
+
+            for (int x = 0; x < width; x++)
+            {
+                mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int y = 0; y < height; y++)
+            {
+                mainGrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            lifeGrid = new LifeGrid(width, height);
+            cells = new Border[width, height];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Border border = new Border();
+                    border.Margin = new Thickness(1);
+                    border.Background = new SolidColorBrush(Colors.LightGray);
+                    border.Tag = new int[] { x, y };
+                    cells[x, y] = border;
+
+                    border.MouseLeftButtonDown += (s, e) =>
+                    {
+                        Border b = s as Border;
+                        int[] tag = b.Tag as int[];
+                        bool currentStatus = lifeGrid.GetCell(tag[0], tag[1]);
+                        lifeGrid.SetCell(tag[0], tag[1], !currentStatus);
+                        cells[tag[0], tag[1]].Background = !currentStatus ? new SolidColorBrush(Colors.Black) : new SolidColorBrush(Colors.LightGray);
+                    };
+
+                    Grid.SetRow(border, x);
+                    Grid.SetColumn(border, y);
+                    mainGrid.Children.Add(border);
+                }
+            }
+
+            lifeGrid.OnUpdate += (newGrid) =>
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        Dispatcher.Invoke(() =>
+                        {
+                            cells[x, y].Background = newGrid.GetCell(x, y) ? new SolidColorBrush(Colors.Black) : new SolidColorBrush(Colors.LightGray);
+                        });
+                    }
+                }
+            };
+
+            autoTickThread = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        if (autoTick)
+                        {
+                            lifeGrid.Update();
+                        }
+                        int i = 250;
+                        Dispatcher.Invoke(() =>
+                        {
+                            i = (int)sliderAutoTickRate.Value;
+                        });
+                        Thread.Sleep(i);
+                    }
+                }
+                catch
+                {
+
+                }
+            });
+            autoTickThread.Start();
+        }
+
+        private void btnTick_Click(object sender, RoutedEventArgs e)
+        {
+            lifeGrid.Update();
+        }
+
+        private void btnAuto_Click(object sender, RoutedEventArgs e)
+        {
+            autoTick = !autoTick;
+            btnAuto.Content = "Turn auto tick " + (autoTick ? "off" : "on");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            autoTickThread.Abort();
+        }
+
+        private void sliderAutoTickRate_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (lblAutoTickRate != null)
+                {
+                    lblAutoTickRate.Content = ((int)sliderAutoTickRate.Value).ToString();
+                }
+            });
+        }
+    }
+}
